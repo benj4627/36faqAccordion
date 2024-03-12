8cf419810c7643643ebbca1961e4758dc7103b39
benj4627 <112342046+benj4627@users.noreply.github.com>
2024-03-12 16:32:35 +0100

    sd

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 //Fanger alle .accordionTitle elementer. De bliver tildelt variablen accordionEl.
 let accordionsEl = document.querySelectorAll('.accordionTitle');
-let plusImgsEl = document.querySelectorAll('.plusSign img');
+// let plusImgsEl = document.querySelectorAll('.plusSign img');
 
+//looper igennem accordions og tildeler click event. 
+//Når man klikker, skiftes der mellem class .active, som viser content af accordion. 
 accordionsEl.forEach((accordionEl) => {
   accordionEl.addEventListener('click', () => {
     accordionEl.classList.toggle('active');
