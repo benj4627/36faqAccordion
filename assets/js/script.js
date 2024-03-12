b3248f6674118379a0111f7573b81b1c11d738d9
benj4627 <112342046+benj4627@users.noreply.github.com>
2024-03-12 16:16:31 +0100

    finished JS

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+//Fanger alle .accordionTitle elementer. De bliver tildelt variablen accordionEl.
+let accordionsEl = document.querySelectorAll('.accordionTitle');
+let plusImgsEl = document.querySelectorAll('.plusSign img');
+
+accordionsEl.forEach((accordionEl) => {
+  accordionEl.addEventListener('click', () => {
+    accordionEl.classList.toggle('active');
+
+    // if (accordionItem.classList.contains('active')) {
+    //     plusSignImg.src = './assets/images/icon-minus.svg';
+    //   } else {
+    //     plusSignImg.src = './assets/images/icon-plus.svg';
+    //   }
+  });
+});
